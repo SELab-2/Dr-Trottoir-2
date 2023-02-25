9c0754a184809cd1125ad1419712503181833e01
BrentBlanckaert <brentblanckaert@outlook.com>
2023-02-25 16:23:31 +0100

    start login frontend

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Layout from "@/components/layout";
+
+export default function Login(){
+    return (
+        <Layout>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <h1></h1>
+        </Layout>)
+}
