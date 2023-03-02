f8843de9e319fc6e05f3bd740c752c35c03091cf
Michiel Lachaert <michiel.lachaert@ugent.be>
2023-03-03 00:16:26 +0100

    fix linter issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,14 @@
 /** @type {import("next").NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: 1,
-  webpackDevMiddleware: config => {
+  reactStrictMode: true,
+  /*webpackDevMiddleware: config => {
     config.watchOptions = {
       poll: 800,
       aggregateTimeout: 300,
     };
     return config;
-  }
+  }*/
 };
 
 module.exports = nextConfig;
