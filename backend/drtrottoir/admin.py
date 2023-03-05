4ab67a9bb22b532d7d9104d67481832406fc36ab
renevds <rene.v.d.s.9@gmail.com>
2023-03-05 22:48:59 +0100

    Register admin

--- backend/drtrottoir/admin.py
+++ backend/drtrottoir/admin.py
@@ -4,7 +4,17 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
-from drtrottoir.models import CustomUser
+
+from .models import (
+    CustomUser,
+    Location,
+    Building,
+    Photo,
+    Visit,
+    Tour,
+    Schedule,
+    BuildingInTour
+)
 
 
 class UserCreationForm(forms.ModelForm):
@@ -78,3 +88,6 @@ admin.site.register(CustomUser, UserAdmin)
 # ... and, since we're not using Django's built-in permissions,
 # unregister the Group model from admin.
 admin.site.unregister(Group)
+# Register other moments
+models = [Location, Building, Photo, Visit, Tour, Schedule, BuildingInTour]
+admin.site.register(models)
