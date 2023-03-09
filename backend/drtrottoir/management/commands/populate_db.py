75c0e3eef8d87019246241143bb05786362d483d
milachae <michiel.lachaert@ugent.be>
2023-03-09 21:00:33 +0100

    #55: delete custom commands, fixtures are a lot easier

--- backend/drtrottoir/management/commands/populate_db.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-class Command(BaseCommand):
-    help = 'Fill the database with dummy data'
-
-    def handle(self, *args, **options):
-
-        self.stdout.write("abc")
