38f98f7a7ec5a95e8b15d663a13fbb28cde1327c
milachae <michiel.lachaert@ugent.be>
2023-03-09 20:58:02 +0100

    #55: added test that checks if data is imported correctly

--- backend/drtrottoir/tests/test_fixtures.py
+++ backend/drtrottoir/tests/test_fixtures.py
@@ -1,21 +1,12 @@
 from django.test import TestCase
-from ..models import (
-    CustomUser,
-    Location,
-    Building,
-    Photo,
-    Visit,
-    Tour,
-    Schedule,
-    BuildingInTour
-)
+from ..models import Building
 
 
 class FixturesTest(TestCase):
     fixtures = ['fixtures/init_data.json']
 
+    # Testing if fixtures are correctly imported
     def test_data_available(self):
         building = Building.objects.get(pk=1)
-        print(building)
         self.assertIsNotNone(building)
-        # A test that uses the fixtures.
+        self.assertTrue(building.name == "Academie")
