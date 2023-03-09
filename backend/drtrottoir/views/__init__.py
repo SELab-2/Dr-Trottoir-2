1549b02224f138802cd962d38e7873101f74d9b3
Jasper Janin <jasper.janin@ugent.be>
2023-03-09 01:06:44 +0100

    endpoints for buildings (with their location info) and locations by themselves

--- backend/drtrottoir/views/__init__.py
+++ backend/drtrottoir/views/__init__.py
@@ -1,4 +1,4 @@
 from .building_viewset import BuildingViewSet
 from .location_viewset import LocationViewSet
-from .email_backend import EmailBackend, login
+from .email_backend import EmailBackend
 from .login import login
