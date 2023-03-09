cac80f36051e3c8d24e009d29f6858103f1950fe
Jasper Janin <jasper.janin@ugent.be>
2023-03-09 01:02:45 +0100

    endpoints for buildings (with their location info) and locations by themselves

--- backend/drtrottoir/serializers/building_serializer.py
+++ backend/drtrottoir/serializers/building_serializer.py
@@ -1,12 +1,13 @@
 from drtrottoir.models import Building
 from rest_framework import serializers
+from .location_serializer import LocationSerializer
 
 
 class BuildingSerializer(serializers.HyperlinkedModelSerializer):
     """
-    A serializer for buildings
+    A serializer for buildings and their associated location
     """
+    location = LocationSerializer(read_only=True)
     class Meta:
         model = Building
         fields = ['url', 'nickname', 'description', 'name', 'manual', 'location']
-
