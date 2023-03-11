af6365405e9ecb400312e76b12178a1ed081ff5e
milachae <michiel.lachaert@ugent.be>
2023-03-11 22:10:35 +0100

    fix test for CI

--- frontend/__tests__/index.test.jsx
+++ frontend/__tests__/index.test.jsx
@@ -1,17 +1,17 @@
 //TODO: Just an example, to be deleted
 
 import { render, screen } from '@testing-library/react'
-import Home from '../src/pages/index'
+import Login from '../src/pages/index'
 import '@testing-library/jest-dom'
 
 describe('Home', () => {
     it('renders a heading', () => {
-        render(<Home />)
+        render(<Login />)
 
-        const heading = screen.getByRole('heading', {
-            name: /René\./i,
+        const el = screen.getByRole('button', {
+            name: "Log in",
         })
 
-        expect(heading).toBeInTheDocument()
+        expect(el).toBeInTheDocument()
     })
 })
