ff258e8b46d619d28e6674ad78f2497a1b51b39a
Brent Blanckaert <125136419+BrentBlanckaert@users.noreply.github.com>
2023-03-12 18:32:11 +0100

    Update backend/drtrottoir/serializers/tour_serializer.py
    Co-authored-by: Jasper <55832482+JasperJanin@users.noreply.github.com>

--- backend/drtrottoir/serializers/tour_serializer.py
+++ backend/drtrottoir/serializers/tour_serializer.py
@@ -7,7 +7,6 @@ class TourSerializer(serializers.HyperlinkedModelSerializer):
     """
     A serializer for tours and their associated location
     """
-    #region = serializers.PrimaryKeyRelatedField(queryset=Region.objects.all())
     region_name = serializers.CharField(source='region.region_name', read_only=True)
 
     class Meta:
