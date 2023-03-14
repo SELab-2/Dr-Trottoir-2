a5d092c94e2fea26ace4e89acb66f5d1faac6593
milachae <michiel.lachaert@ugent.be>
2023-03-14 13:32:48 +0100

    #57: fix visit field in photo model

--- backend/drtrottoir/models/photo.py
+++ backend/drtrottoir/models/photo.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-from .building import Building
+from .visit import Visit
 
 IMAGE_STATES = ((1, 'Arrival'), (2, 'Departure'), (3, 'Extra'))
 
 
 class Photo(models.Model):
     # image = models.ImageField(verbose_name="image", upload_to="images/") Pillow needs to be set up
-    visit = models.ForeignKey(Building, verbose_name="id of visit", on_delete=models.CASCADE)
+    visit = models.ForeignKey(Visit, verbose_name="id of visit", on_delete=models.CASCADE)
     state = models.IntegerField(verbose_name="type of photo", choices=IMAGE_STATES)
     comment = models.TextField(verbose_name="comment on the photo")
     created_at = models.DateTimeField(verbose_name="time of creation")
 
     def __str__(self):
-        return f"{self.visit.name}, {self.created_at}"
+        return f"{self.visit}, {self.created_at}"
