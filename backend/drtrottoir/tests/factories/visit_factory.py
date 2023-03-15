27eb9bb02337c929f6503cc52f0501d20d45c7ed
BrentBlanckaert <brentblanckaert@outlook.com>
2023-03-15 10:16:09 +0100

    fixed warning

--- backend/drtrottoir/tests/factories/visit_factory.py
+++ backend/drtrottoir/tests/factories/visit_factory.py
@@ -1,6 +1,6 @@
 from factory.django import DjangoModelFactory
 import factory
-
+from django.utils import timezone
 from drtrottoir.models import Visit
 from .building_factory import BuildingFactory
 from .user_factory import DeveloperUserFactory
@@ -9,7 +9,7 @@ from .user_factory import DeveloperUserFactory
 class VisitFactory(DjangoModelFactory):
     user = factory.SubFactory(DeveloperUserFactory)
     building = factory.SubFactory(BuildingFactory)
-    arrival = factory.Faker("date_time")
+    arrival = factory.Faker("date_time", tzinfo=timezone.utc)
     comment = factory.Faker("sentence")
 
     class Meta:
