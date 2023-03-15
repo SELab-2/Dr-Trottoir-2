c1f92a0dfd7678fce01e270b187bcfeaeb4ae735
renevds <rene.v.d.s.9@gmail.com>
2023-03-15 15:28:32 +0100

    Add tests

---
+++ backend/drtrottoir/tests/views/test_register.py
@@ -0,0 +1,23 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+from drtrottoir.models import CustomUser
+from drtrottoir.serializers import VisitSerializer
+from drtrottoir.tests.factories import VisitFactory, DeveloperUserFactory
+
+
+class TestRegisterView(APITestCase):
+    """ Test module for GET single Region API """
+
+    def test_get(self):
+        data = {
+            "email": "test123@test123.com",
+            "password": "testtest123",
+            "password2": "testtest123",
+            "first_name": "first",
+            "last_name": "last"
+        }
+        response = self.client.post(reverse("auth_register"), data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(CustomUser.objects.filter(email=data['email']).exists())
