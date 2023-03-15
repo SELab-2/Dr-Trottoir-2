6f9c82659ff8ffc856a4ff1280e7ae2f7c016698
Wout Welvaert <72563573+wwelvaer@users.noreply.github.com>
2023-03-15 14:32:31 +0100

    Update backend/drtrottoir/serializers/waste_serializer.py
    Co-authored-by: Jasper <55832482+JasperJanin@users.noreply.github.com>

--- backend/drtrottoir/serializers/waste_serializer.py
+++ backend/drtrottoir/serializers/waste_serializer.py
@@ -1,11 +1,13 @@
 from drtrottoir.models import Waste
 from rest_framework import serializers
+from .building_partial import BuildingPartialSerializer
 
 
 class WasteSerializer(serializers.HyperlinkedModelSerializer):
     """
     A serializer for waste model
     """
+    building = BuildingPartialSerializer(read_only=True)
 
     class Meta:
         model = Waste
