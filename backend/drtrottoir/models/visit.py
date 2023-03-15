4dae559b31b3126578d6fae75b5f91411551ca51
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-03-15 18:15:44 +0100

    changed visit model

--- backend/drtrottoir/models/visit.py
+++ backend/drtrottoir/models/visit.py
@@ -6,9 +6,9 @@ from .custom_user import CustomUser
 
 class Visit(models.Model):
     arrival = models.DateTimeField(verbose_name="time of arrival")
-    building_in_tour = models.ForeignKey(BuildingInTour, verbose_name="id of building", on_delete=models.CASCADE)
+    building_in_tour = models.ForeignKey(BuildingInTour, verbose_name="id of building in tour", on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, verbose_name="id of user", on_delete=models.CASCADE)
     comment = models.TextField(verbose_name="Comment on the visit", blank=True)
 
     def __str__(self):
-        return f"{self.user.first_name}, {self.building.building.nickname}: {self.arrival}"
+        return f"{self.user.first_name}, {self.building_in_tour.building.nickname}: {self.arrival}"
