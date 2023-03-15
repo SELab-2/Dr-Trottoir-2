1da0adc3caf6e70c8a7acfa90ae10c9969a4867a
Bert Cappelle <bert.cappelle@ugent.be>
2023-03-15 23:29:02 +0100

    str model fix + test buildingInTour

--- backend/drtrottoir/tests/views/test_building_in_tour.py
+++ backend/drtrottoir/tests/views/test_building_in_tour.py
@@ -3,7 +3,7 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 
 from drtrottoir.serializers import BuildingInTourSerializer
-from drtrottoir.tests.factories import BuildingInTourFactory, DeveloperUserFactory
+from drtrottoir.tests.factories import BuildingInTourFactory, BuildingFactory, TourFactory, DeveloperUserFactory
 
 class TestBuildingInTourView(APITestCase):
     """
@@ -11,6 +11,8 @@ class TestBuildingInTourView(APITestCase):
     """
 
     def setUp(self):
+        self.tour = TourFactory()
+        self.building = BuildingFactory()
         self.buildingInTour = BuildingInTourFactory()
         user = DeveloperUserFactory()
         self.client.force_authenticate(user=user)
@@ -21,3 +23,6 @@ class TestBuildingInTourView(APITestCase):
         self.assertEqual(serializer.data, response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_post(self):
+        response = self.client.post('/api/building_in_tour/', data={"tour": self.tour.pk, "building": self.building.pk, "order_index": 1}, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- backend/drtrottoir/views/building_in_tour_viewset.py
+++ backend/drtrottoir/views/building_in_tour_viewset.py
@@ -9,27 +9,8 @@ from rest_framework.response import Response
 
 class BuildingInTourViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows buildings to be viewed or edited.
+    API endpoint that allows buildings ina specific tour to be viewed or edited.
     """
     queryset = BuildingInTour.objects.all()
     serializer_class = BuildingInTourSerializer
     permission_classes = [IsAuthenticated & SuperPermissionOrReadOnly]
-    
-    def create(self, request):
-        data = request.data
-        serializer = self.get_serializer_class()
-
-        new_building_in_tour = BuildingInTour.objects.create(tour=data["tour"], building=data["building"],
-                                                  order_index=data["order_index"])
-        ser = serializer(instance=new_building_in_tour, context={'request': request})
-        return Response({'building_in_tour': ser.data})
-    
-    @action(detail=True, methods=['delete'])
-    def delete(self, request, pk=None):
-        building_in_tour = BuildingInTour.objects.filter(pk=pk)
-        if building_in_tour.exists():
-            building_in_tour.delete()
-            return Response(status=204)
-        else:
-            return Response({'message': 'The given building_in_tour does not exist'}, status=404)
-            
