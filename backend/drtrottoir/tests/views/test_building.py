bbb092e945ca0f8fc7ad88059becf2f41e4b784c
Wout Welvaert <wout.welvaert1@gmail.com>
2023-03-16 12:03:56 +0100

    fixed linter issues

--- backend/drtrottoir/tests/views/test_building.py
+++ backend/drtrottoir/tests/views/test_building.py
@@ -55,7 +55,10 @@ class TestBuildingView(APITestCase):
         self.assertEqual(len(r.data), 2)
         self.assertFalse(data_future["waste_type"] in map(lambda x: x["waste_type"], r.data))
         # Start and End param
-        r = self.client.get(f'/api/building/{self.building.pk}/waste?start={data_now["date"]}&end={data_now["date"]}', follow=True)
+        r = self.client.get(
+            f'/api/building/{self.building.pk}/waste?start={data_now["date"]}&end={data_now["date"]}',
+            follow=True
+        )
         self.assertEqual(r.status_code, status.HTTP_200_OK)
         self.assertEqual(len(r.data), 1)
         self.assertEqual(r.data[0]["waste_type"], data_now["waste_type"])
@@ -70,7 +73,10 @@ class TestBuildingView(APITestCase):
         self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
         r = self.client.get(f'/api/building/{self.building.pk}/waste?end=0000-00-00', follow=True)
         self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
-        r = self.client.get(f'/api/building/{self.building.pk}/waste?date={data_now["date"]}&start={data_past["date"]}', follow=True)
+        r = self.client.get(
+            f'/api/building/{self.building.pk}/waste?date={data_now["date"]}&start={data_past["date"]}', 
+            follow=True
+        )
         self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
         # Wrong building id
         r = self.client.get(f'/api/building/-1/waste', follow=True)
--- backend/drtrottoir/views/building_viewset.py
+++ backend/drtrottoir/views/building_viewset.py
@@ -47,20 +47,29 @@ class BuildingViewSet(viewsets.ModelViewSet):
             # Catch unknown parameters
             unknown_params = list(filter(lambda x: x not in params, q_param_keys))
             if unknown_params:
-                return Response(f"Unknown query params: {', '.join(unknown_params)}. Valid params: {params}", status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    f"Unknown query params: {', '.join(unknown_params)}. Valid params: {params}", 
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
             if "date" in q_param_keys:
                 # Catch wrong usage of date param
                 if len(q_param_keys) > 1:
-                    return Response("Can't combine 'date' parameter with another parameter", status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        "Can't combine 'date' parameter with another parameter", 
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
 
                 try:
                     q = Waste.objects.filter(date=q_params["date"]).order_by('id')
                     # Return WastePartialSerializer
                     return Response(WastePartialSerializer(list(q), many=True, context={'request': request}).data)
 
-                except: # Catch ivalid date
-                    return Response("Invalid date: date must be of format (YYYY-MM-DD)", status=status.HTTP_400_BAD_REQUEST)
+                except Exception:  # Catch ivalid date
+                    return Response(
+                        "Invalid date: date must be of format (YYYY-MM-DD)", 
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
 
             # Start and End params
             try:
@@ -70,8 +79,11 @@ class BuildingViewSet(viewsets.ModelViewSet):
                 # Return WastePartialSerializer
                 return Response(WastePartialSerializer(list(q), many=True, context={'request': request}).data)
 
-            except: # Catch ivalid dates
-                return Response("Invalid date: date must be of format (YYYY-MM-DD)", status=status.HTTP_400_BAD_REQUEST)
-                
+            except Exception: # Catch ivalid dates
+                return Response(
+                    "Invalid date: date must be of format (YYYY-MM-DD)", 
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+  
         else:
             return Response("Given building doesn't exist.", status=status.HTTP_400_BAD_REQUEST)
