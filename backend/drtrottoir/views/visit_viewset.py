061edd85f6aaabdf284d619f0919f9ba3712eec4
milachae <michiellachaert@outlook.com>
2023-03-16 19:02:07 +0100

    photos available to visit

--- backend/drtrottoir/views/visit_viewset.py
+++ backend/drtrottoir/views/visit_viewset.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets
+from django.urls import reverse
+from rest_framework import viewsets, status, serializers
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from drtrottoir.models import Visit
 from drtrottoir.permissions.user_permissions import SuperPermissionOrReadOnly
 from drtrottoir.serializers import VisitSerializer
+from drtrottoir.models import Photo, Building
+from drtrottoir.serializers import PhotoSerializer
 
 
 class VisitViewSet(viewsets.ModelViewSet):
@@ -29,3 +35,19 @@ class VisitViewSet(viewsets.ModelViewSet):
     queryset = Visit.objects.all()
     serializer_class = VisitSerializer
     permission_classes = [IsAuthenticated & SuperPermissionOrReadOnly]
+
+    @action(detail=True, methods=['get'])
+    def photos(self, request, pk=None):
+        """
+        Get all photos inside a visit. Authentication required.
+        """
+        if pk is not None and Visit.objects.filter(pk=pk).exists():
+
+            urls = []
+            for photo in Photo.objects.filter(visit=pk):
+                url = reverse('photo-detail', args=[photo.id])
+                urls.append(request.build_absolute_uri(url))
+
+            return Response({"photos": urls})
+        else:
+            return Response("Given visit doesn't exist.", status=status.HTTP_400_BAD_REQUEST)
