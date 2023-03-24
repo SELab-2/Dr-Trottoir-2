5f4817c418baa3607b3d8f3b42842fd1afc808ec
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-03-24 18:04:51 +0100

    :sparkles: made a progressbar component #126

---
+++ frontend/src/components/Progressbar.js
@@ -0,0 +1,38 @@
+import {useEffect, useState} from "react";
+
+export default function Progressbar({finishedCount, amount}){
+	const low = "#CF222E"
+	const average = "#D29922"
+	const high = "#2DA44C"
+	const done = "#8250DF"
+	const [percentage, setPercentage] = useState(0);
+	const [color, setColor] = useState(low);
+
+
+	useEffect(() => {
+		let value = Math.floor((finishedCount/amount)*100)
+
+		if (value <= 100){
+			if ( value < 33) {
+				setColor(low)
+			}else if (value < 66){
+				setColor(average)
+			}else if (value < 99){
+				setColor(high)
+			}else {
+				setColor(done)
+			}
+		}else {
+			value = 0
+			setColor(low)
+		}
+		setPercentage(value);
+
+    },[]);
+
+	return (
+		<div className={"bg-gray-100 p-1 rounded"}>
+			<div className={"py-1 px-1 rounded"} style={{width: `${percentage}%`, backgroundColor: color}}/>
+		</div>
+	)
+}
