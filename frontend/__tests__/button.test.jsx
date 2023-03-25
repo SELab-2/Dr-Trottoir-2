8eb039dfbc24199b3672a5d4ac295ad02b0d320f
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-03-25 17:08:08 +0100

    :sparkles: added extra test for SecondaryButton component #161

--- frontend/__tests__/button.test.jsx
+++ frontend/__tests__/button.test.jsx
@@ -1,12 +1,22 @@
 import {render, screen, waitFor, fireEvent} from '@testing-library/react';
-import {PrimaryButton} from "@/components/Button";
+import {PrimaryButton, SecundaryButton} from "@/components/Button";
 import {
 	faCirclePlus,
 } from '@fortawesome/free-solid-svg-icons'
 
-test('calls handle function on button press', async () => {
+test('calls handle function on button press in PrimaryButton component', async () => {
   const handleClick = jest.fn();
   render(<PrimaryButton icon={faCirclePlus} text={"click me"} handle={handleClick}/>);
+  const button = screen.getByRole('button', {name: /click me/i});
+  await waitFor(() => {
+    fireEvent.click(button);
+  });
+  expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+test('calls handle function on button press in SecondaryButton component', async () => {
+  const handleClick = jest.fn();
+  render(<SecundaryButton icon={faCirclePlus} text={"click me"} handle={handleClick}/>);
   const button = screen.getByRole('button', {name: /click me/i});
   await waitFor(() => {
     fireEvent.click(button);
