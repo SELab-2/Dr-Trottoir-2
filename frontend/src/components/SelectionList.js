a0f5c20a5889bff58320c2665fa5ce17e62522aa
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-03-25 17:04:49 +0100

    :sparkles: Started work on Selection list #152

---
+++ frontend/src/components/SelectionList.js
@@ -0,0 +1,9 @@
+
+
+export default function SelectionList({title}){
+	return (
+		<div className={"h-full p-4 w-1/6 overflow-y-auto rounded-lg bg-gray-100"}>
+			<h1 className={"font-semibold"}>{title}</h1>
+		</div>
+	)
+}
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,6 +3,7 @@ import {getSession, signOut} from "next-auth/react"
 import {useState} from "react";
 import {BuildingService} from "@/services/building.service";
 import {UserService} from "@/services/user.service";
+import SelectionList from "@/components/SelectionList";
 
 export default function Home() {
 	const [response, setResponse] = useState('{}');
@@ -28,23 +29,26 @@ export default function Home() {
 				<div className={"mb-20"}>
 					<p className={"text-xl font-bold"}>Home.</p>
 				</div>
-				<div>
+				<div className={"flex flex-row space-x-4"}>
 					<div>
-						<h2 className={"text-lg font-bold pb-3"}>Test authentication</h2>
-						<p>
-							If you are viewing this page, you are successfully logged in <span className={"emoji"}>🥳</span>
-						</p>
-						<button className={buttonStyle} onClick={() => signOut()}>log out</button>
+						<div>
+							<h2 className={"text-lg font-bold pb-3"}>Test authentication</h2>
+							<p>
+								If you are viewing this page, you are successfully logged in <span className={"emoji"}>🥳</span>
+							</p>
+							<button className={buttonStyle} onClick={() => signOut()}>log out</button>
+						</div>
+						<div className={"pt-10"}>
+							<h2 className={"text-lg font-bold pb-3"}>Test models</h2>
+							<button className={buttonStyle} onClick={allBuildings}> All buildings </button>
+							<button className={buttonStyle} onClick={allUsers}> All users </button>
+						</div>
+						<h2 className={"text-lg font-bold mt-10"}>Response</h2>
+						<div className={"mt-3 mb-10 text-gray-800 bg-gray-200 min-w-full w-fit rounded-lg p-5"}>
+							<pre> {response} </pre>
+						</div>
 					</div>
-					<div className={"pt-10"}>
-						<h2 className={"text-lg font-bold pb-3"}>Test models</h2>
-						<button className={buttonStyle} onClick={allBuildings}> All buildings </button>
-						<button className={buttonStyle} onClick={allUsers}> All users </button>
-					</div>
-					<h2 className={"text-lg font-bold mt-10"}>Response</h2>
-					<div className={"mt-3 mb-10 text-gray-800 bg-gray-200 min-w-full w-fit rounded-lg p-5"}>
-						<pre> {response} </pre>
-					</div>
+					<SelectionList title={"Rondes"}/>
 				</div>
 				<div className={"py-12"}>
 					<p>By team 2 </p>
