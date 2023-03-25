e531140ab2d03bab843e73ac953de8f8d6d5572f
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-03-25 17:27:33 +0100

    test

--- frontend/src/components/SelectionList.js
+++ frontend/src/components/SelectionList.js
@@ -1,9 +1,10 @@
 
 
-export default function SelectionList({title}){
+export default function SelectionList({title, Component, callback,}){
 	return (
-		<div className={"h-full p-4 w-1/6 overflow-y-auto rounded-lg bg-gray-100"}>
+		<div className={"h-full p-4 w-1/6 rounded-lg overflow-y-auto bg-gray-100"}>
 			<h1 className={"font-semibold"}>{title}</h1>
+			<Component/>
 		</div>
 	)
 }
