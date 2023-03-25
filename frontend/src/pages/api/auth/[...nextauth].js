8aca04e8edc63eb7b968d074ee7ce96b63abdde4
renevds <rene.v.d.s.9@gmail.com>
2023-03-25 21:05:00 +0100

    :wrench: Configure eslint and prettierrc

--- frontend/src/pages/api/auth/[...nextauth].js
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -3,94 +3,91 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 
-
-// TODO(Elias): add secret (https://next-auth.js.org/v3/configuration/options#secret)
-
 export default async function auth(req, res) {
-	const baseServerUrl = 'http://django:8000/api/'
+	const baseServerUrl = "http://django:8000/api/";
 
 	const refreshAccessToken = async (token) => {
 		try {
-			const response = await axios.post(baseServerUrl + 'user/auth/refresh/', {refresh: token.refresh})
+			const response = await axios.post(baseServerUrl + "user/auth/refresh/", { refresh: token.refresh });
 
 			if (response.status !== 200) {
-				return null
+				return null;
 			}
 
-			const { access, refresh } = response.data
-			const { exp } = jwtDecode(access)
+			const { access, refresh } = response.data;
+			const { exp } = jwtDecode(access);
 
-			return {access: access, refresh: refresh, exp: exp, user: token.user}
+			return { access: access, refresh: refresh, exp: exp, user: token.user };
 		} catch (error) {
-			return null
+			return null;
 		}
-	}
+	};
 
-	const jwt = async ({token, user, account, profile, isNewUser}) => {
+	const jwt = async ({ token, user, account, profile, isNewUser }) => {
 		if (account && user) {
-			return user
+			return user;
 		}
-		if (Date.now()/1000 >= token.exp) {
-			token = await refreshAccessToken(token)
+		if (Date.now() / 1000 >= token.exp) {
+			token = await refreshAccessToken(token);
 		}
-		return token
-	}
+		return token;
+	};
 
-	const session = async ({session, token}) => {
+	const session = async ({ session, token }) => {
 		if (!token) {
-			return session
+			return session;
 		}
-		session.user = token.user
-		session.access = token.access
-		session.refresh = token.refresh
-		session.exp = token.exp
-		return session
-	}
+		session.user = token.user;
+		session.access = token.access;
+		session.refresh = token.refresh;
+		session.exp = token.exp;
+		return session;
+	};
 
 	const providers = [
 		CredentialsProvider({
-			name: 'Login',
-			id: 'mail-login',
-			type: 'credentials',
+			name: "Login",
+			id: "mail-login",
+			type: "credentials",
 			credentials: {
-				email: {label: "email", type: "text"},
-				password: {label: "password", type: "password"},
+				email: { label: "email", type: "text" },
+				password: { label: "password", type: "password" },
 			},
-			async authorize({email, password}, req) {
+			async authorize({ email, password }, req) {
 
-				let response = await axios.post(  baseServerUrl + 'user/auth/', {email, password})
+				let response = await axios.post(baseServerUrl + "user/auth/", { email, password });
 
 				if (response.status !== 200) {
-					return null
+					return null;
 				}
 
-				const { access, refresh } = response.data
-				const { exp } = jwtDecode(access)
+				const { access, refresh } = response.data;
+				const { exp } = jwtDecode(access);
 
-				const authHeader = {Authorization: 'Bearer ' + access}
-				response = await axios.get( baseServerUrl + 'user/me/', {headers: authHeader})
+				const authHeader = { Authorization: "Bearer " + access };
+				response = await axios.get(baseServerUrl + "user/me/", { headers: authHeader });
 				if (response.status !== 200) {
-					return null
+					return Promise.reject(new Error(response?.errors));
 				}
-				const user = response.data
+				const user = response.data;
 
-				return {access: access, refresh: refresh, exp: exp, user: user}
+				return { access: access, refresh: refresh, exp: exp, user: user };
 			},
-		})
-	]
+		}),
+	];
 
 	const options = {
 		providers: providers,
-		session: { strategy: "jwt", jwt: true},
+		session: { strategy: "jwt", jwt: true },
 		callbacks: { jwt, session },
 		secret: process.env.NEXTAUTH_SECRET,
 		pages: {
-			singIn: '/',
-			singOut: '/',
-			error: '/',
+			singIn: "/",
+			singOut: "/",
+			error: "/",
 		},
-	}
+	};
 
-	return await NextAuth(req, res, options)
+	return await NextAuth(req, res, options);
 }
 
