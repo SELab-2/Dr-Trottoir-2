10fde9452625ad2af6f8fe551f245553aba68edf
milachae <michiellachaert@outlook.com>
2023-03-26 17:42:10 +0200

    :poop: remove start colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-	  extend: {
-		  colors: {
-			  s2condPurple: '#a32eff',
-			  secondary: "#E6E600",
-			  primary: "#FFFFFF"
-		  }
-	  },
   },
   plugins: [],
 }
