285b7ae50e16de6b59e28c2d1e249a1ac1782575
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-03-28 22:12:25 +0200

    :recycle: Cleaned code #152

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -1,42 +1,53 @@
-import {BG_BAD, BG_MEH, BG_GOOD, BG_DONE, DONE, BAD, MEH, GOOD} from "@/utils/colors";
+import {
+  BG_BAD,
+  BG_MEH,
+  BG_GOOD,
+  BG_DONE,
+  DONE,
+  BAD,
+  MEH,
+  GOOD,
+} from "@/utils/colors";
 import ProgressBar from "react-customizable-progressbar";
 
-export default function CustomProgressBar({finishedCount, amount, wheel}){
-	let percentage = (finishedCount/amount)*100
-	let color = BAD
-	let bgColor = BG_BAD
+export default function CustomProgressBar({ finishedCount, amount, wheel }) {
+  let percentage = (finishedCount / amount) * 100;
+  let color = BAD;
+  let bgColor = BG_BAD;
 
-	if ( percentage >= 33 && percentage <= 100) {
-		if (percentage < 66){
-			color = MEH
-			bgColor = BG_MEH
-		}else if (percentage < 99){
-			color = GOOD
-			bgColor = BG_GOOD
-		}else {
-			color = DONE
-			bgColor = BG_DONE
-		}
-	}else if (percentage < 0 || percentage > 100){
-		percentage = 0
-	}
+  if (percentage >= 33 && percentage <= 100) {
+    if (percentage < 66) {
+      color = MEH;
+      bgColor = BG_MEH;
+    } else if (percentage < 99) {
+      color = GOOD;
+      bgColor = BG_GOOD;
+    } else {
+      color = DONE;
+      bgColor = BG_DONE;
+    }
+  } else if (percentage < 0 || percentage > 100) {
+    percentage = 0;
+  }
 
-	if (wheel){
-		return (
-			<ProgressBar
-				progress={percentage}
-				radius={100}
-				strokeColor={color}
-				trackStrokeWidth={35}
-				strokeWidth={20}
-			/>
-		)
-	}else {
-		return (
-		<div className={"p-1 rounded"} style={{backgroundColor: bgColor}}>
-			<div className={"py-1 px-1 rounded"} style={{width: `${percentage}%`, backgroundColor: color}}/>
-		</div>
-	)
-	}
-
+  if (wheel) {
+    return (
+      <ProgressBar
+        progress={percentage}
+        radius={100}
+        strokeColor={color}
+        trackStrokeWidth={35}
+        strokeWidth={20}
+      />
+    );
+  } else {
+    return (
+      <div className={"p-1 rounded"} style={{ backgroundColor: bgColor }}>
+        <div
+          className={"py-1 px-1 rounded"}
+          style={{ width: `${percentage}%`, backgroundColor: color }}
+        />
+      </div>
+    );
+  }
 }
--- frontend/src/components/SelectionList.js
+++ frontend/src/components/SelectionList.js
@@ -1,32 +1,57 @@
-import {BG_LIGHT_SECONDARY, BG_ACCENT} from "@/utils/colors";
-import {useState} from "react";
+import { BG_LIGHT_SECONDARY, BG_ACCENT } from "@/utils/colors";
+import { useState } from "react";
 
-function Selection({list, Component, callback}){
-	const defaultBG = BG_LIGHT_SECONDARY
-	const selectedBG = BG_ACCENT
-	const [selected, setSelected] = useState("")
+function Selection({ list, Component, callback }) {
+  const defaultBG = BG_LIGHT_SECONDARY;
+  const selectedBG = BG_ACCENT;
+  const [selected, setSelected] = useState("");
 
-	return (
-			<>
-				{list.map((entry) => {
-					if (entry["url"] === selected) {
-						return (<Component key={entry["url"]} background={selectedBG} setSelected={(string) => {setSelected(string)}} callback={callback} data={entry}/>)
-					}else {
-						return (<Component key={entry["url"]} background={defaultBG} setSelected={(string) => {setSelected(string)}} callback={callback} data={entry}/>)
-					}
-				})}
-			</>
-		)
+  return (
+    <>
+      {list.map((entry) => {
+        if (entry["url"] === selected) {
+          return (
+            <Component
+              key={entry["url"]}
+              background={selectedBG}
+              setSelected={(string) => {
+                setSelected(string);
+              }}
+              callback={callback}
+              data={entry}
+            />
+          );
+        } else {
+          return (
+            <Component
+              key={entry["url"]}
+              background={defaultBG}
+              setSelected={(string) => {
+                setSelected(string);
+              }}
+              callback={callback}
+              data={entry}
+            />
+          );
+        }
+      })}
+    </>
+  );
 }
 
-export default function SelectionList({title, Component, callback, elements}){
-	console.log(elements)
-	return (
-		<div className={"h-full p-5 w-1/6 flex flex-col rounded-lg bg"}>
-			<h1 className={"font-semibold py-2"}>{title}</h1>
-			<div className="h-full overflow-auto space-y-2 rounded-lg">
-				<Selection list={elements} Component={Component} callback={callback}/>
-			</div>
-		</div>
-	)
+export default function SelectionList({
+  title,
+  Component,
+  callback,
+  elements,
+}) {
+  console.log(elements);
+  return (
+    <div className={"h-full p-5 w-1/6 flex flex-col rounded-lg bg"}>
+      <h1 className={"font-semibold py-2"}>{title}</h1>
+      <div className="h-full overflow-auto space-y-2 rounded-lg">
+        <Selection list={elements} Component={Component} callback={callback} />
+      </div>
+    </div>
+  );
 }
