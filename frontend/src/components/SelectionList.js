fc5bba6e2fec2e1a01e6e3a0303168a90f820b3e
milachae <michiellachaert@outlook.com>
2023-03-29 22:35:24 +0200

    :rotating_light: fix linter issues

--- frontend/src/components/SelectionList.js
+++ frontend/src/components/SelectionList.js
@@ -1,6 +1,6 @@
 import { BG_LIGHT_SECONDARY, BG_ACCENT } from "@/utils/colors";
 import { useState } from "react";
-import {PrimaryCard} from "@/components/CustomCard";
+import { PrimaryCard } from "@/components/CustomCard";
 
 function Selection({ list, Component, callback }) {
   const defaultBG = BG_LIGHT_SECONDARY;
