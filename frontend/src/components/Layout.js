c00627161d5f93e1d921a54987859cdb198299c9
milachae <michiellachaert@outlook.com>
2023-04-02 20:01:17 +0200

    :bug: start fixing navbar not visible on login screen

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,33 +2,49 @@ import Head from "next/head";
 import Navbar from "@/components/navbar/Navbar";
 import { useEffect, useState } from "react";
 import { UserService } from "@/services/user.service";
+import { useSession } from "next-auth/react";
 
 export default function Layout({ children }) {
+  const { data: session, status } = useSession();
   const [currentUser, setCurrentUser] = useState(null);
   const [isLoading, setLoading] = useState(false);
 
   // Fetch the data about the current logged in User
   useEffect(() => {
     setLoading(true);
-    UserService.getLoggedInUser().then((data) => {
-      setCurrentUser(data);
-      setLoading(false);
-    });
-  }, []);
+
+    //TODO remove this, error handling should be done in GetLoggedInUser.
+    if (session?.user) {
+      UserService.getLoggedInUser().then((data) => {
+        setCurrentUser(data);
+      });
+    }
+    setLoading(false);
+  }, [session?.user]);
 
   // TODO: change to something usefull
   if (isLoading) return <p>Loading...</p>;
-  if (!currentUser) return <p>No profile data</p>;
+  // if (!currentUser) return <p>No profile data</p>;
+
+  // change position of the screen when no user is logged in
+  let position = "ml-72";
+  if (!currentUser) {
+    position = "";
+  }
 
   return (
     <div className={"h-screen w-screen"}>
       <Head>
         <link rel="icon" href="/favicon_beer.ico" />
       </Head>
-      <div>
-        <Navbar user={currentUser} />
-      </div>
-      <div className={"ml-72"}>
+
+      {session?.user && (
+        <div>
+          <Navbar user={currentUser} />
+        </div>
+      )}
+
+      <div className={position}>
         <main>{children}</main>
       </div>
     </div>
