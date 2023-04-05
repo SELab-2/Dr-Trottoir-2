cb0ceea20903bff951ca3d09e2d00dada0c04642
milachae <michiellachaert@outlook.com>
2023-04-05 16:14:58 +0200

    :recycle: use other library for spinner

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,10 +1,16 @@
-import { ClipLoader } from "react-spinners";
+import { TailSpin } from "react-loader-spinner";
 
-export default function Loading({ className }) {
+export default function Loading({ className, width = 80, height = 80 }) {
   return (
     <div className={`min-h-screen flex flex-col ${className}`}>
       <div className={"relative flex flex-grow justify-center items-center"}>
-        <ClipLoader color={"#000"} size={50} />
+        <TailSpin
+          height={height}
+          width={width}
+          color="#000"
+          radius="1"
+          visible={true}
+        />
       </div>
     </div>
   );
