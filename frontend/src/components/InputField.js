608419fc01f4cc8f3dedbd3cad38f9b3d4f1e815
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-04-05 17:24:31 +0200

    :lipstick: Made requested changes to inputfield #124

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,27 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function CustomInputField({ icon, reference, callback }) {
+export default function CustomInputField({
+  icon,
+  reference,
+  callback,
+  classNameDiv,
+  classNameInput,
+}) {
   return (
     <div
-      className={
-        "rounded-full bg-light-bg-2 p-4 border-solid border-4 border-light-h-2 relative flex items-center"
-      }
+      className={`rounded-full bg-light-bg-2 p-4 border-solid border-4 border-light-h-2 relative flex items-center ${classNameDiv}`}
     >
       <input
         name={"input-field"}
         ref={reference}
         type="text"
-        className="flex-1 bg-light-bg-2 outline-0"
+        className={`flex-1 bg-light-bg-2 outline-none ${classNameInput}`}
       />
-      {icon ? (
+      {icon && (
         <FontAwesomeIcon
           data-testid="input-button"
           onClick={callback}
           className="relative cursor-pointer"
           icon={icon}
         />
-      ) : (
-        <></>
       )}
     </div>
   );
