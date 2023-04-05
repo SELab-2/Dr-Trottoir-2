553a91b238814c0bbdc94f476ef104bcfcc06da6
milachae <michiellachaert@outlook.com>
2023-04-05 12:13:05 +0200

    :truck: rename files + start dashboard page

---
+++ frontend/src/pages/admin/dashboard.js
@@ -0,0 +1,14 @@
+import { PrimaryCard } from "@/components/CustomCard";
+
+
+export default function AdminDashboardPage() {
+  // TODO: Implement this page
+
+  return (
+    <div>
+      <PrimaryCard class_style={" border-2 border-accent-1"}>
+        <p>test</p>
+      </PrimaryCard>
+    </div>
+  );
+}
