70629111f5cdcea2ccdcd5ed6686bec3641faacd
renevds <rene.v.d.s.9@gmail.com>
2023-04-06 22:17:27 +0200

    :recycle: Split selection

---
+++ frontend/src/components/selection/SelectionList.js
@@ -0,0 +1,21 @@
+import { PrimaryCard } from "@/components/custom-card/PrimaryCard";
+import Selection from "@/components/selection/Selection";
+
+export default function SelectionList({
+  title,
+  Component,
+  callback,
+  elements,
+}) {
+  return (
+    <PrimaryCard
+      title={title}
+      text={""}
+      className={"h-full w-1/6 p-5 flex flex-col p-5"}
+    >
+      <div className="h-full overflow-auto space-y-2 rounded-lg">
+        <Selection list={elements} Component={Component} callback={callback} />
+      </div>
+    </PrimaryCard>
+  );
+}
