6249bc31372a13542e4d59c830790eff4d2198f1
renevds <rene.v.d.s.9@gmail.com>
2023-04-06 22:09:34 +0200

    :recycle: Split pickers

---
+++ frontend/src/components/CustomDayPicker.js
@@ -0,0 +1,19 @@
+import DatePicker from "react-datepicker";
+import { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+
+export function CustomDayPicker() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+      dateFormat="dd/MM/yyyy"
+      calendarStartDay={1}
+      className={
+        "rounded-full w-1/6 p-2 border-2 border-light-h-2 bg-light-bg-2"
+      }
+    />
+  );
+}
