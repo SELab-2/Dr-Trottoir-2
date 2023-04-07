e21fbe962a0bc6469f31ac7fba12633285f82f9a
Bert Cappelle <bert.cappelle@ugent.be>
2023-04-07 10:46:24 +0200

    🚨Fix linter warnings

--- frontend/src/components/ProfilePicture.js
+++ frontend/src/components/ProfilePicture.js
@@ -1,10 +1,14 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export function ProfilePicture({srcPath, width, height}){
-    
-    return (
-        <div className="relative m-2" style={{ width: width, height: height}}>
-            <Image src={srcPath} alt="Profile picture" layout={width && height ? "fill" : "responsive"} objectFit="cover" className="rounded-full"/>
-        </div>
-    );
+export function ProfilePicture({ srcPath, width, height }) {
+  return (
+    <div className="relative m-2" style={{ width: width, height: height }}>
+      <Image
+        src={srcPath}
+        alt="Profile picture"
+        className="rounded-full object-cover"
+        style={{ aspectRatio: `${width}/${height}` }}
+      />
+    </div>
+  );
 }
--- frontend/src/components/Tag.js
+++ frontend/src/components/Tag.js
@@ -1,10 +1,10 @@
-export function ColoredTag({text, color}) { 
-    return (
-      <div 
-        className={"inline-block text-center px-2 py-1 rounded-full text-sm"} 
-        style={{backgroundColor: color}}
-      >
-        {text}
-      </div>
-    );
+export function ColoredTag({ text, color }) {
+  return (
+    <div
+      className={"inline-block text-center px-2 py-1 rounded-full text-sm"}
+      style={{ backgroundColor: color }}
+    >
+      {text}
+    </div>
+  );
 }
