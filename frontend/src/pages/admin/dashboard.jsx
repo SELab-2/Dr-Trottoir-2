ff498a67e6110f115f5ebf6c568a1c6d229fa02c
milachae <michiellachaert@outlook.com>
2023-04-08 19:29:03 +0200

    :construction: fix merge errors

--- frontend/src/pages/admin/dashboard.jsx
+++ frontend/src/pages/admin/dashboard.jsx
@@ -1,12 +1,71 @@
-import Layout from "@/components/Layout";
+import Head from "next/head";
+import CustomButton from "@/components/button/Button";
+import PrimaryButton from "@/components/button/PrimaryButton";
+import SecondaryButton from "@/components/button/SecondaryButton";
+import PrimaryCard from "@/components/custom-card/PrimaryCard";
+import SecondaryCard from "@/components/custom-card/SecondaryCard";
+import CustomInputField from "@/components/input-fields/InputField";
+import {
+  faCalendarDay,
+  faFilter,
+  faMagnifyingGlass,
+  faPlusCircle, faSort,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function AdminDashboardPage() {
   // TODO: Implement this page
+
   return (
     <>
-      <div className={"h-screen"}>
-        <p>placeholder</p>
+      <Head>
+        <title>Dasboard</title>
+      </Head>
+
+      <div className={"flex m-2"}>
+        <SecondaryButton text={"WeekPicker (Brent)"} icon={faCalendarDay} />
       </div>
+
+      <PrimaryCard>
+        <div id={"statistics"} className={"flex flex-row"}>
+          <SecondaryCard title={"Aantal Rondes"} className={"flex-grow"}>
+            <p>Aantal rondes</p>
+          </SecondaryCard>
+          <SecondaryCard title={"Aantal opmerkingen"} className={"flex-grow"}>
+            <p>aantal opmerkingen</p>
+          </SecondaryCard>
+          <SecondaryCard title={"Overview"} className={"flex-grow"}>
+            <p>Overview</p>
+          </SecondaryCard>
+        </div>
+
+        <div id={"rondes"}>
+          <SecondaryCard title={"Rondes"}>
+            <PrimaryCard>
+              <div className={"flex flex-row justify-center items-center"}>
+                <div className={"flex items-center px-2 items-center"}>
+                  <PrimaryButton text={"Filter"} icon={faFilter} />
+                </div>
+
+                <div className={"px-2"}>
+                  <PrimaryButton text={"Sort"} icon={faSort} />
+                </div>
+
+                <div className={"flex-grow px-2"}>
+                  <CustomInputField icon={faMagnifyingGlass} />
+                </div>
+
+                <div className={"px-2"}>
+                  <PrimaryButton text={"Nieuw"} icon={faPlusCircle} />
+                </div>
+              </div>
+            </PrimaryCard>
+
+            <PrimaryCard>
+              <p>Lijst</p>
+            </PrimaryCard>
+          </SecondaryCard>
+        </div>
+      </PrimaryCard>
     </>
   );
 }
