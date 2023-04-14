b51511e6f3bac728af778a2f38db61819fc8525e
renevds <rene.v.d.s.9@gmail.com>
2023-04-14 03:51:44 +0200

    :construction: work on Table

---
+++ frontend/src/components/table/Cell.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Cell({ children, cut }) {
+  const [showFull, setShowFull] = useState(false);
+
+  const cutClassNames =
+    "max-w-[150px] overflow-hidden text-ellipsis whitespace-nowrap";
+
+  return (
+    <div className={"relative"}>
+      <div
+        className={`p-1 text-light-text ${cut ? cutClassNames : ""}`}
+        onMouseOver={() => setShowFull(true)}
+      >
+        {children}
+      </div>
+      {cut && showFull && (
+        <div
+          className={
+            "absolute z-10 left-0 top-0 bg-light-bg-2 border-b-2 border-r-2 border-l-2 p-1"
+          }
+          onMouseOut={() => setShowFull(false)}
+        >
+          {children}
+        </div>
+      )}
+    </div>
+  );
+}
