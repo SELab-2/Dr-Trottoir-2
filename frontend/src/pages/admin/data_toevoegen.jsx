25117355ae2f6036e301d5f348806680db1de6f5
milachae <michiellachaert@outlook.com>
2023-04-15 19:09:58 +0200

    :lipstick: general layout page

--- frontend/src/pages/admin/data_toevoegen.jsx
+++ frontend/src/pages/admin/data_toevoegen.jsx
@@ -1,6 +1,19 @@
 import Head from "next/head";
 import PrimaryCard from "@/components/custom-card/PrimaryCard";
 import CustomButton from "@/components/button/Button";
+import {
+  faBicycle,
+  faBriefcase,
+  faBuilding,
+  faCalendarWeek,
+  faPeopleGroup,
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import NavbarList from "@/components/navbar/NavbarList";
+import PrimaryButton from "@/components/button/PrimaryButton";
+import SecondaryButton from "@/components/button/SecondaryButton";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function AdminDataAddPage() {
   // TODO: Implement this page
@@ -9,9 +22,30 @@ export default function AdminDataAddPage() {
       <Head>
         <title>Data toevoegen/bewerken:</title>
       </Head>
-      <div>
+      <div className={"flex flex-row h-full w-full p-2 space-x-2"}>
+        <div className={`w-1/5 space-y-2`}>
+          <PrimaryCard title={"Data types"}>
+            <div className={"flex flex-col"}>
+              <Link href={"#"}>Planning</Link>
+              <Link href={"#"}>Rondes</Link>
+              <Link href={"#"}>Gebouwen</Link>
+              <Link href={"#"}>Personeel</Link>
+              <Link href={"#"}>Syndici</Link>
+            </div>
+          </PrimaryCard>
+          <PrimaryButton icon={faPlusCircle} className={"w-full"}>
+            Nieuw Item
+          </PrimaryButton>
+          <SecondaryButton icon={faPlusCircle} className={"w-full"}>
+            Kopieer Item
+          </SecondaryButton>
+        </div>
+        <PrimaryCard className={`h-full w-1/5`} title={"Huidige"}></PrimaryCard>
 
-        <p>placeholder</p>
+        <PrimaryCard
+          className={`h-full w-3/5`}
+          title={"Bewerken"}
+        ></PrimaryCard>
       </div>
     </>
   );
