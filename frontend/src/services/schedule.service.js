884bcee2aff93bd1694b5f25767b5a86a7c30e26
milachae <michiellachaert@outlook.com>
2023-04-16 18:12:38 +0200

    solve merge conflicts

--- frontend/src/services/schedule.service.js
+++ frontend/src/services/schedule.service.js
@@ -1,17 +1,6 @@
 import ApiInstance from "@/services/ApiInstance";
 
 class ScheduleService {
-<<<<<<< HEAD
-  async getAll() {
-    let all = [];
-    let response;
-    try {
-      response = await ApiInstance.getApi().get("schedule/");
-    } catch (e) {
-      alert(JSON.stringify(e, null, 2));
-      return []; // TODO: what to do when no data can be fetched.
-    }
-=======
   /**
    * Return the data of a get request of a page.
    * When an error occurs, an alert will be showen and the error will be returned.
@@ -39,36 +28,10 @@ class ScheduleService {
   async getSchedules(args = {}) {
     let all = [];
     let response = await ApiInstance.getApi().get("schedule/");
->>>>>>> origin/development
 
     if (response.status === 200) {
       all = response.data.results;
       while (response.data.next != null) {
-<<<<<<< HEAD
-        const response = await ApiInstance.getApi().get(response.data.next);
-        all.push(...response.data.results);
-      }
-
-      return all;
-    }
-
-    return [];
-  }
-
-  async getVisitsFromSchedule(id) {
-    let response;
-    try {
-      response = await ApiInstance.getApi().get(`schedule/${id}/visits/`);
-    } catch (e) {
-      alert(JSON.stringify(e, null, 2));
-      return []; // TODO: what to do when no data can be fetched.
-    }
-
-    if (response.status === 200) {
-      return response.data;
-    }
-    return [];
-=======
         const response = await this.getPage(response.data.next);
         if (response.status === 200) {
           all.push(...response.data.results);
@@ -123,7 +86,6 @@ class ScheduleService {
       data = data.filter((schedule) => args.tours.includes(schedule.tour));
     }
     return data;
->>>>>>> origin/development
   }
 }
 
