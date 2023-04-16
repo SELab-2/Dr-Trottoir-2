58e1fcdc76c14ca75bf5b54d4b8f08412d8067b2
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-04-16 19:30:24 +0200

    :construction: changed user service

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -5,6 +5,21 @@ class UserService {
     const response = await ApiInstance.getApi().get("user/");
     return response.data;
   }
+
+  async getUserById(id) {
+    let response;
+    try {
+      response = await ApiInstance.getApi().get(`user/${id}/`);
+    } catch (e) {
+      alert(JSON.stringify(e, null, 2));
+      return []; // TODO: what to do when no data can be fetched.
+    }
+
+    if (response.status === 200) {
+      return response.data;
+    }
+    return [];
+  }
 }
 
 export default new UserService();
