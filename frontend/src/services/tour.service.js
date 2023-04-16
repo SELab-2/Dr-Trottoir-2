39d22c1567e1a2ec8bd910b12a8c4b4d3e7d4cf9
brentblanckaert@outlook.com <Brent.Blanckaert@UGent.be>
2023-04-16 19:29:09 +0200

    :construction: changed tour service

--- frontend/src/services/tour.service.js
+++ frontend/src/services/tour.service.js
@@ -1,9 +1,36 @@
 import ApiInstance from "@/services/ApiInstance";
 
 class TourService {
+  /**
+   * Return the data of a get request of a page.
+   * When an error occurs, an alert will be showen and the error will be returned.
+   * @param url The URL where you want to perform a get request on.
+   * @returns {Promise<*>}
+   */
+  async getPage(url) {
+    let response = null;
+    try {
+      response = await ApiInstance.getApi().get(url);
+    } catch (e) {
+      response = e;
+      alert(JSON.stringify(e.message, null, 2));
+    }
+    return response;
+  }
+
   async getAll() {
-    const response = await ApiInstance.getApi().get("tour/");
-    return response.data;
+    let response = await this.getPage(`tour/`);
+    return response.status === 200 ? response.data : [];
+  }
+
+  /**
+   * Returns all the building IDs for a specific tour.
+   * @param id The ID of the schedule you want the visits of.
+   * @returns {Promise<*|*[]>}
+   */
+  async getBuildingsFromTour(id) {
+    let response = await this.getPage(`tour/${id}/buildings/`);
+    return response.status === 200 ? response.data : [];
   }
 }
 
