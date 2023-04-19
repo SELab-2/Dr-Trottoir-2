defa2a66a0cfa94b6c7d465621382b9eb93ecacd
Elias Nijs <elias.nijs@gmail.com>
2023-04-19 17:59:02 +0200

    :adhesive_bandage:fixed eslint issue

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,6 +1,8 @@
 export default function ColoredTag({ className, children }) {
   return (
-    <div className={`${className} whitespace-nowrap rounded-full px-3 py-1 m-1 w-fit `}>
+    <div
+      className={`${className} whitespace-nowrap rounded-full px-3 py-1 m-1 w-fit `}
+    >
       {children}
     </div>
   );
