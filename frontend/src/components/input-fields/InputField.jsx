6378f2b0231b73b0dc592164da3557b2476cb689
Jasper Janin <jasper.janin@ugent.be>
2023-04-19 23:48:31 +0200

    ⚡️ InputField Style + Keypress callback

--- frontend/src/components/input-fields/InputField.jsx
+++ frontend/src/components/input-fields/InputField.jsx
@@ -1,26 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+/**
+ * Custom component for an input field, option for an icon on the right side.
+ * @param icon Some FontAwesome icon
+ * @param reference A react ref to communicate input state
+ * @param iconCallback Function that will be called when the icon is clicked
+ * @param keyDownCallback function that will be called when a key is pressed
+ * @param classNameDiv className that will be added to the enclosing div
+ * @param classNameInput className that will be added to the <input/> member
+ */
 export default function CustomInputField({
   icon,
   reference,
-  callback,
+  iconCallback,
+  keyDownCallback,
   classNameDiv,
   classNameInput,
 }) {
   return (
     <div
-      className={`rounded-lg bg-light-bg-2 p-4 border-solid border-2 border-light-h-2 relative flex items-center ${classNameDiv}`}
+      className={`rounded-lg bg-light-bg-2 p-1 border-solid border-2 border-light-h-2 relative flex items-center ${classNameDiv}`}
     >
       <input
         name={"input-field"}
         ref={reference}
         type="text"
+        onKeyDown={keyDownCallback}
         className={`flex-1 bg-light-bg-2 outline-none ${classNameInput}`}
       />
       {icon && (
         <FontAwesomeIcon
           data-testid="input-button"
-          onClick={callback}
+          onClick={iconCallback}
           className="relative cursor-pointer"
           icon={icon}
         />
