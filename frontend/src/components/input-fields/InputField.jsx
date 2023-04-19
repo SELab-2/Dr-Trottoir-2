9c4ad7bde6c9ca88db86db745558fe48280f9733
Jasper Janin <jasper.janin@ugent.be>
2023-04-20 00:26:48 +0200

    ✨ onActionCallback option for input fields, listening to both icon clicks and enter presses

--- frontend/src/components/input-fields/InputField.jsx
+++ frontend/src/components/input-fields/InputField.jsx
@@ -4,8 +4,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
  * Custom component for an input field, option for an icon on the right side.
  * @param icon Some FontAwesome icon
  * @param reference A react ref to communicate input state
+ * @param onActionCallback Function that will be called when either enter is pressed or the icon is clicked
  * @param iconCallback Function that will be called when the icon is clicked
- * @param keyDownCallback function that will be called when a key is pressed
+ * @param keyDownCallback Function that will be called when a key is pressed
  * @param classNameDiv className that will be added to the enclosing div
  * @param classNameInput className that will be added to the <input/> member
  */
@@ -14,9 +15,23 @@ export default function CustomInputField({
   reference,
   iconCallback,
   keyDownCallback,
+  onActionCallback,
   classNameDiv,
   classNameInput,
 }) {
+
+  const combinedKeyCallback = (event) => {
+    if (event.key === "Enter" && onActionCallback !== undefined) {
+      onActionCallback(event);
+    }
+    if (keyDownCallback !== undefined) keyDownCallback(event);
+  }
+
+  const combinedIconCallback = (event) => {
+    if (iconCallback !== undefined) iconCallback(event);
+    if (onActionCallback !== undefined) onActionCallback(event);
+  }
+
   return (
     <div
       className={`rounded-lg bg-light-bg-2 p-1 border-solid border-2 border-light-h-2 relative flex items-center ${classNameDiv}`}
@@ -25,13 +40,14 @@ export default function CustomInputField({
         name={"input-field"}
         ref={reference}
         type="text"
-        onKeyDown={keyDownCallback}
+        onKeyDown={combinedKeyCallback}
+        
         className={`flex-1 bg-light-bg-2 outline-none ${classNameInput}`}
       />
       {icon && (
         <FontAwesomeIcon
           data-testid="input-button"
-          onClick={iconCallback}
+          onClick={combinedIconCallback}
           className="relative cursor-pointer"
           icon={icon}
         />
