85089db48d7f52f5b84fc2f393597eab42d9baa7
milachae <michiellachaert@outlook.com>
2023-04-20 21:54:23 +0200

    :sparkles: added visuals

--- frontend/src/pages/admin/data_toevoegen.jsx
+++ frontend/src/pages/admin/data_toevoegen.jsx
@@ -1,6 +1,12 @@
 import { useRouter } from "next/router";
+import AdminDashboardPage from "@/pages/admin/planning";
+import Layout from "@/components/Layout";
 
 export default function AdminDataAddPage() {
   const router = useRouter();
   router.push("/admin/data_toevoegen/planning");
 }
+
+AdminDataAddPage.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
