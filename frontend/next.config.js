fdb2134c47e84eadb21a977783124880656a4627
renevds <rene.v.d.s.9@gmail.com>
2023-04-20 21:23:22 +0200

    :bug: Fix maps key

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,12 +8,6 @@ const nextConfig = {
     };
     return config;
   },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
-  },
   async rewrites() {
     return [{ source: "/next/:path*", destination: "/api/:path*" }];
   },
