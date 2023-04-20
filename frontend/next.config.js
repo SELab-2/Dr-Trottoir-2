f2e54d5de983b98d9acd1e6f4247f4df8a2bf71a
renevds <rene.v.d.s.9@gmail.com>
2023-04-20 21:15:37 +0200

    :bug: Fix maps key

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,22 @@
 /** @type {import("next").NextConfig} */
 
 const nextConfig = {
-	reactStrictMode: true,
-	webpack: config => {
-		config.watchOptions = {
-			poll: 800,
-			aggregateTimeout: 300,
-		};
-		return config;
-	},
-	async rewrites() {
-		return [{source: '/next/:path*', destination: '/api/:path*'}]
-	},
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 800, aggregateTimeout: 300
+    };
+    return config;
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+  },
+  async rewrites() {
+    return [{ source: "/next/:path*", destination: "/api/:path*" }];
+  },
 };
 
 module.exports = nextConfig;
