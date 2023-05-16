f2a62a6d5fc8ccb4c02ff74b449141818215c978
Jasper Janin <jasper.janin@me.com>
2023-05-16 19:30:36 +0200

    ✏️ nederlands

--- frontend/src/pages/beheer/templates.jsx
+++ frontend/src/pages/beheer/templates.jsx
@@ -216,7 +216,7 @@ export default function Templates() {
                 className={"mr-2"}
                 options={[]}
               >
-                Sort
+                Sorteer
               </Dropdown>
               <InputField
                 classNameDiv={"w-80"}
