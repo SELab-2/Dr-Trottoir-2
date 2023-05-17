7f6d12d924a6193017b84dfb52e01629918a92af
Jasper Janin <jasper.janin@ugent.be>
2023-05-18 01:32:56 +0200

    🚧 building comment endpoint

--- backend/drtrottoir/views/building_viewset.py
+++ backend/drtrottoir/views/building_viewset.py
@@ -3,9 +3,15 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from drtrottoir.models import Building, Waste, Photo
+from drtrottoir.models import Building, Waste, Photo, ScheduleComment, VisitComment
 from drtrottoir.permissions.user_permissions import SuperPermissionOrReadOnly
-from drtrottoir.serializers import BuildingSerializer, WastePartialSerializer, PhotoSerializer
+from drtrottoir.serializers import (
+    BuildingSerializer,
+    WastePartialSerializer,
+    PhotoSerializer,
+    ScheduleCommentSerializer,
+    VisitCommentSerializer
+)
 
 
 class BuildingViewSet(viewsets.ModelViewSet):
@@ -120,6 +126,65 @@ class BuildingViewSet(viewsets.ModelViewSet):
                 q = Photo.objects.filter(**arg).filter(visit__building_in_tour__building__pk=pk).order_by('created_at')
                 # Return WastePartialSerializer
                 return Response(PhotoSerializer(list(q), many=True, context={'request': request}).data)
+
+            except Exception as e:  # Catch ivalid dates
+                return Response(
+                    f"{e}",
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        else:
+            return Response("Given building doesn't exist.", status=status.HTTP_400_BAD_REQUEST)
+
+    # Get comments of a building
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        # Check if building id is valid
+        if pk is not None and Building.objects.filter(pk=pk).exists():
+            q_params = request.query_params
+            q_param_keys = q_params.keys()
+            params = [
+                "start",
+                "end",
+            ]
+
+            # Catch unknown parameters
+            unknown_params = list(filter(lambda x: x not in params, q_param_keys))
+            if unknown_params:
+                return Response(
+                    f"Unknown query params: {', '.join(unknown_params)}. Valid params: {params}",
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            # Start and End params
+            try:
+                # Adds date__gte field when start param is present and date__gte field when end param is present
+                arg = {
+                    "created_at__date__gte" if k == "start" else "created_at__date__lte":
+                    datetime.date.fromisoformat(q_params[k])
+                    for k in q_param_keys
+                }
+                qs = (
+                    ScheduleComment
+                    .objects
+                    .filter(**arg)
+                    .filter(building__pk=pk)
+                    .order_by('created_at')
+                )
+                qv = (
+                    VisitComment
+                    .objects
+                    .filter(**arg)
+                    .filter(visit__building_in_tour__building__pk=pk)
+                    .order_by('created_at')
+                )
+                scheduleCommentData = ScheduleCommentSerializer(list(qs), many=True, context={'request': request}).data
+                for comment in scheduleCommentData:
+                    comment["type"] = "schedule_comment"
+                visitCommentData = VisitCommentSerializer(list(qv), many=True, context={'request': request}).data
+                for comment in visitCommentData:
+                    comment["type"] = "visit_comment"
+                return Response(scheduleCommentData + visitCommentData)
 
             except Exception as e:  # Catch ivalid dates
                 return Response(
