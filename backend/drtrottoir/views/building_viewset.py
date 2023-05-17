6bf74832cb23ce4fb5e0ab7ae0741984d129ec21
Jasper Janin <jasper.janin@ugent.be>
2023-05-17 23:36:43 +0200

    ✨ building/[x]/photos endpoint

--- backend/drtrottoir/views/building_viewset.py
+++ backend/drtrottoir/views/building_viewset.py
@@ -1,10 +1,11 @@
+import datetime
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from drtrottoir.models import Building, Waste
+from drtrottoir.models import Building, Waste, Photo
 from drtrottoir.permissions.user_permissions import SuperPermissionOrReadOnly
-from drtrottoir.serializers import BuildingSerializer, WastePartialSerializer
+from drtrottoir.serializers import BuildingSerializer, WastePartialSerializer, PhotoSerializer
 
 
 class BuildingViewSet(viewsets.ModelViewSet):
@@ -82,6 +83,47 @@ class BuildingViewSet(viewsets.ModelViewSet):
             except Exception:  # Catch ivalid dates
                 return Response(
                     "Invalid date: date must be of format (YYYY-MM-DD)",
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        else:
+            return Response("Given building doesn't exist.", status=status.HTTP_400_BAD_REQUEST)
+
+    # Get photos of a building
+    @action(detail=True, methods=['get'])
+    def photos(self, request, pk=None):
+        # Check if building id is valid
+        if pk is not None and Building.objects.filter(pk=pk).exists():
+            q_params = request.query_params
+            q_param_keys = q_params.keys()
+            params = [
+                "start",
+                "end",
+            ]
+
+            # Catch unknown parameters
+            unknown_params = list(filter(lambda x: x not in params, q_param_keys))
+            if unknown_params:
+                return Response(
+                    f"Unknown query params: {', '.join(unknown_params)}. Valid params: {params}",
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            # Start and End params
+            try:
+                # Adds date__gte field when start param is present and date__gte field when end param is present
+                arg = {
+                    "created_at__date__gte" if k == "start" else "created_at__date__lte":
+                    datetime.date.fromisoformat(q_params[k])
+                    for k in q_param_keys
+                }
+                q = Photo.objects.filter(**arg).order_by('created_at')
+                # Return WastePartialSerializer
+                return Response(PhotoSerializer(list(q), many=True, context={'request': request}).data)
+
+            except Exception as e:  # Catch ivalid dates
+                return Response(
+                    f"{e}",
                     status=status.HTTP_400_BAD_REQUEST
                 )
 
