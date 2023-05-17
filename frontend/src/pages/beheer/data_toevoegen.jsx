2f451870b85138a1209101ae59a3da83a7dac833
Elias Nijs <elias.nijs@gmail.com>
2023-05-17 15:58:51 +0200

    :bug: fix redirect from data_toevoegen to data_toevoegen/planningen

--- frontend/src/pages/beheer/data_toevoegen.jsx
+++ frontend/src/pages/beheer/data_toevoegen.jsx
@@ -1,11 +1,18 @@
-import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
 
 export default function AdminDataAddPage() {
-  const router = useRouter();
-  router.push("/beheer/data_toevoegen/planningen");
+  return <></>;
 }
 
 AdminDataAddPage.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/beheer/data_toevoegen/planningen",
+      permanent: false,
+    },
+  };
+}
