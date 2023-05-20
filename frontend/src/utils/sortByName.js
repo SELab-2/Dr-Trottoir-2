21df2ef0eee58f9236b262d7bad5bcb5072dc69d
renevds <rene.v.d.s.9@gmail.com>
2023-05-21 01:26:18 +0200

    :bug: Fix SortByName

--- frontend/src/utils/sortByName.js
+++ frontend/src/utils/sortByName.js
@@ -1,7 +1,7 @@
-export default function sortByName(arr) {
+export default function sortByName(arr, key = "name") {
   return arr.sort((a, b) => {
-    const aName = a.name;
-    const bName = b.name;
+    const aName = a[key]?.toLowerCase();
+    const bName = b[key]?.toLowerCase();
     if (aName < bName) {
       return -1;
     }
